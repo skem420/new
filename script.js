e427be9c30fd89e1d3acf1bcb39e14f457637f73
skem <90285038+skem420@users.noreply.github.com>
2023-05-13 20:24:42 +0100

    Update and rename script.js to text.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const background = document.getElementById('background');
-const changeBackgroundBtn = document.getElementById('change-background-btn');
-
-changeBackgroundBtn.addEventListener('click', () => {
-  background.style.backgroundImage = 'url("https://www.w3schools.com/html/mov_bbb.mp4")';
-  background.style.backgroundSize = 'cover';
-});
